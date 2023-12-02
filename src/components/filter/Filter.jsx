27fd95a62d42a9cc93e11aba9536700e8ea0d8ba
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-12-03 00:03:29 +0530

    3-12-2023

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -5,7 +5,14 @@ function Filter() {
     const context = useContext(myContext)
     const { mode, searchkey, setSearchkey } = context
 
-    
+    return (
+        <div>
+            <div className=' container mx-auto px-4 mt-5 '>
+                
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Filter
