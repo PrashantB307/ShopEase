31a63f56141151ab74165741466ff6b2e6f77d8c
PrashantB-307 <pb28.rkt@gmail.com>
2024-01-27 20:28:15 +0530

    :fire: Project Complete :fire:

--- src/components/herosection/HeroSection.jsx
+++ src/components/herosection/HeroSection.jsx
@@ -2,7 +2,7 @@
 function HeroSection() {
   return (
     <div>
-        <img src="https://static.vecteezy.com/system/resources/previews/004/299/835/original/online-shopping-on-phone-buy-sell-business-digital-web-banner-application-money-advertising-payment-ecommerce-illustration-search-free-vector.jpg" alt="" />
+        <img src="../../../public/banner.jpg" width="100%" alt="" />
     </div>
   )
 }
