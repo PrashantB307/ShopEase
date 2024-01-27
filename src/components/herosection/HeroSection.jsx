08a1d66a98f8f9d27d024ee782b78a630e7406f5
Prashant Kumar <119515910+PrashantB307@users.noreply.github.com>
2024-01-28 01:01:54 +0530

    Update HeroSection.jsx

--- src/components/herosection/HeroSection.jsx
+++ src/components/herosection/HeroSection.jsx
@@ -2,7 +2,7 @@
 function HeroSection() {
   return (
     <div>
-        <img src="../../../public/shoppingBanner2.jpg" width="100%" alt="" />
+        <img src="https://static.vecteezy.com/system/resources/thumbnails/004/299/815/small/online-shopping-on-phone-buy-sell-business-digital-web-banner-application-money-advertising-payment-ecommerce-illustration-search-vector.jpg" width="100%" alt="" />
     </div>
   )
 }
